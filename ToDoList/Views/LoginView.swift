a7b51c8b68a4df5e6653dbdbd8cbad3bc0619d6c
lancecho <lancecho@umich.edu>
2025-04-07 16:49:58 +0900

    MainView

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -8,9 +8,49 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        NavigationView{
+            VStack{ // Vertical Stack
+                // Header
+                HeaderView(title:"To Do List", subtitle: "Get things done", angle: 15, background: .pink)
+                
+                // Login Form
+                Form{
+                    TextField("Email Address", text: $email)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    SecureField("Password", text: $email)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    
+                    Button {
+                        // Attempt log in
+                    } label: {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(Color.blue)
+                            
+                            Text("Log In")
+                                .foregroundColor(Color.white)
+                                .bold()
+                        }
+                    }
+                }
+                .offset(y: -50)
+                
+                // Create Accout
+                VStack{
+                    Text("New around here?")
+                    NavigationLink("Create An Account", destination: RegisterView())
+                }
+                .padding(.bottom, 50)
+                
+                Spacer() // To push things up
+            }
+        }
+        }
 }
 
 #Preview {
